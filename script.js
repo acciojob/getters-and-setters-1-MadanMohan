f30159decb617174b7929a9eba2512d969af3df9
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-07-22 19:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
+//complete this code
 class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.ageValue = age; // using a different internal variable to support getter/setter
-  }
+	constructor(name, number){
+		this.name = name;
+		this.number = number;
+	}
 
-  // Getter for name
-  get name() {
-    return this.nameValue;
-  }
-
-
-
-  // Setter for age
-  set age(newAge) {
-    this.ageValue = newAge;
-  }
+		get name(){
+			return this.name;
+		}
+		set age(number){
+			this.number = number;
+		}
 }
 
 class Student extends Person {
-  study() {
-    console.log(`${this.name} is studying`);
-  }
+	study(){
+		console.log(`${this.name} is studying`)}
 }
 
 class Teacher extends Person {
-  teach() {
-    console.log(`${this.name} is teaching`);
-  }
+		teach(){
+		console.log(`${this.name} is teaching`)}
 }
-
+}
 
 // Do not change the code below this line
 window.Person = Person;
